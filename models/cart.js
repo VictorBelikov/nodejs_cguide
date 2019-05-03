cfa599a8fc5540c5b5c1ae3ea739d959274ca193
Victor Belikov <v1ctor.belikov@gmail.com>
2019-05-03 15:10:22 +0300

    feat: 9.2

--- models/cart.js
+++ models/cart.js
@@ -6,7 +6,7 @@ const rootDir = require('../util/path');
 const filePath = path.join(rootDir, 'data/cart.json');
 
 class Cart {
-  static addProduct(id) {
+  static addProduct(id, productPrice) {
     fs.readFile(filePath, (err, data) => {
       if (err) {
         console.log(err);
@@ -15,8 +15,17 @@ class Cart {
       const cart = data.toString() ? JSON.parse(data) : { products: [], totalPrice: 0 };
       const existingProduct = cart.products.find((p) => p.id === id);
 
+      let updatedProduct;
       if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.quantity++;
+      } else {
+        updatedProduct = { id, quantity: 1 };
+        cart.products = [...cart.products, updatedProduct];
       }
+
+      cart.totalPrice += productPrice;
+      fs.writeFile(filePath, JSON.stringify(cart), (e) => console.log(e));
     });
   }
 }
