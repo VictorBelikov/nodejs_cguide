8bfeba07f1e8dd90e244ce65f119b46ba03fd859
Victor Belikov <v1ctor.belikov@gmail.com>
2019-05-13 12:36:48 +0300

    feat: 11.1

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-// const expressHbs = require('express-handlebars');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errController = require('./controllers/error');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -25,14 +25,19 @@ app.use(shopRoutes);
 // Если ни один из маршрутов не будет обработан
 app.use(errController.get404Page);
 
+// Просматривает все созданные нами модели затем создает соответств. таблицы в БД
+sequelize
+  .sync()
+  .then((result) => app.listen(3000, () => console.log('Server is running on port 3000...')))
+  .catch((err) => console.log(err));
+
 // ========================= Create server ====================================
 
 // const server = http.createServer(app);
 // server.listen(3000, () => console.log('Server is running on port 3000...'));
 
 // more simple way
-app.listen(3000, () => console.log('Server is running on port 3000...'));
-
+// app.listen(3000, () => console.log('Server is running on port 3000...'));
 // because:
 // app.listen = function listen() {
 //   var server = http.createServer(this);
--- util/database.js
+++ util/database.js
@@ -1,11 +1,23 @@
-const mysql2 = require('mysql2');
+// const mysql2 = require('mysql2');
 
-const pool = mysql2.createPool({
+// const pool = mysql2.createPool({
+//   port: 3307,
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'node-complete',
+//   password: '1111111831V1ctoR',
+// });
+
+// module.exports = pool.promise();
+
+// ============================================================================
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', '1111111831V1ctoR', {
+  dialect: 'mysql',
   port: 3307,
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: '1111111831V1ctoR',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
